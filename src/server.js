453d3eca565fe27c232c2e5040ec0f38ecf7234d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-03 12:07:34 -0300

    Iniciando com Node.js - node --watch

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res)=>{
-    return res.end('Hello word')
+    return res.end('Hello word - Isso é um teste')
 })
 
 server.listen(3333)
