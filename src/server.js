b1a8d8253c21870157f47c387b2e02c9edf0084d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-05 14:53:34 -0300

    Feat: Streams no Node.js - Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 
 // - Criar usuários
@@ -38,39 +39,24 @@ import http from 'node:http'
 const users = []
 
 const server = http.createServer(async (req, res) => {
-  
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-
     users.push({
       id: 1,
       name,
       email,
     })
-
     return res.writeHead(201).end()
   }
   return res.writeHead(404).end()
 })
-
 server.listen(3333)
