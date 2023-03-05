84f708f12dd6a78eff5d1e8f6ba488a4dfc1190f
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-05 14:45:23 -0300

    Feat: Streams no Node.js - Entendendo Buffers no Node.js

---
+++ streams/buffer.js
@@ -0,0 +1,7 @@
+const buf = Buffer.from("hello")
+
+console.log(buf.toJSON())
+
+
+// Buffer é o meio nativo do node de 'trocar/tratar/armazenar", informações na memoria. Que logo que é finalizado, é eliminado da memoria.
+
