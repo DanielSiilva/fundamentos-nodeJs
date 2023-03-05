e2f21c22b44d7bced93cbeb6d2884b7a0ad48001
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-04 21:06:52 -0300

    Feat: Streams no Node.js - Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,27 +1,42 @@
-// Streams -> No NodeJs toda porta  de saída e entrada é um streams
-
+// Streams -> Em NodeJs todas as portas de entredas e saídas são streams
 // process.stdin
 //   .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
+// De escrita
 class OneToHundredStream extends Readable {
   index = 1
-
   _read() {
     const i = this.index++
-
     setTimeout(() => {
       if (i > 100) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
-
         this.push(buf)
       }
     }, 1000);
   }
 }
 
+//De transformação
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+//De leitura
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
 new OneToHundredStream()
-    .pipe(process.stdout)
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
