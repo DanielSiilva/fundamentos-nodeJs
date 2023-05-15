39d50728d43fff4ca2f909f4d9f69efd47e3c47b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-15 14:18:08 -0300

    Feat: Criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,13 +1,35 @@
-// Netflix & Spotify
-
-// Importação de clientes via CSV (Excel)
-// 1gb - 1.000.000
-// POST /upload import.csv
-
-// 10mb/s - 100s
-
-// 100s -> Inserção no banco de dados
-
-// 10mb/s -> 10.000
-
 // Readable Streams [lendo] / Writable Streams [enviando ]
+
+// Toda porta de entrada e saída em Node, é uma Streams.
+
+// Streams ->
+
+// stdin => é tudo que o usuario digita no terminal => é uma stream de leitura ;
+// stdout => é uma stream de escrita, saida. 
+// process.stdin
+//   .pipe(process.stdout)
+
+
+//      Readable: stream de leitura 
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+
+        this.push(buf)
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
