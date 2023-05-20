51b8efdc0372f141be33abaa8878f5aafcef1142
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-19 23:34:11 -0300

    Feat: Rotas da aplicação - Filtrando lista do banco de dados

--- src/database.js
+++ src/database.js
@@ -1,15 +1,11 @@
 import fs from 'node:fs/promises'
 
-// import.meta.url : diz qual o caminho do arquivo em questão
-//URl: Recebe 2 parametros, o nome onde queremos criar o arquivo e o caminho relativo onde queremos criar o caminho;
-
 const databasePath = new URL('../db.json', import.meta.url)
+
 
 export class Database {
   #database = {}
-
   constructor() {
-    //
     fs.readFile(databasePath, 'utf8')
       .then(data => {
         this.#database = JSON.parse(data)
@@ -18,14 +14,20 @@ export class Database {
         this.#persist()
       })
   }
-
   #persist() {
-    //writeFile: 2 parametros: caminho do arquivo, e como queremos escrever nele
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
@@ -35,24 +37,18 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
-
     this.#persist()
-
     return data
   }
-
   update(table, id, data) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
-
     if (rowIndex > -1) {
       this.#database[table][rowIndex] = { id, ...data }
       this.#persist()
     }
   }
-
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
-
     if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1)
       this.#persist()
