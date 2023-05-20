d53fe239c214d0b9df93997eaf6d3117be4f74e9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-19 21:01:35 -0300

    Feat: Criando ID único e universal (UUID)

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@
 
 
 import http from 'node:http'
+import {randomUUID} from 'node:crypto'
 import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
@@ -56,7 +57,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
